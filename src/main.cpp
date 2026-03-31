7756821484bd740893d0e69efcfcd5c2c6172159
Santiago Fgueroa <syasaf3389@gmail.com>
2026-03-31 17:39:37 -0500

    Implementacion de la funcion leerConfig que parsea config.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 // Almacena la configuracion leida desde config.txt
@@ -14,27 +16,62 @@ struct ConfigLiga {
 
 // Representa un partido jugado
 struct Game {
-  string date;
-  string local;
-  string visiting;
-  int lgoals;
-  int vgoals;
+    string date;
+    string local;
+    string visiting;
+    int lgoals;
+    int vgoals;
 };
 
 // Representa un equipo en la tabla de posiciones
 struct Team {
-  string name;
-  int tgames;
-  int dgames;
-  int wgames;
-  int lgames;
-  int sgoals;
-  int agoals;
-  int dgoals;
-  int points;
+    string name;
+    int tgames;
+    int wgames;
+    int dgames;
+    int lgames;
+    int sgoals;
+    int agoals;
+    int dgoals;
+    int points;
 };
 
+// Leer la configuracion y asignar el valor al campo correspondiente del struct
+ConfigLiga leerConfig(string ruta){
+    ifstream archivo(ruta);
+    if (archivo.is_open()){
+        ConfigLiga config;
+        string linea;
+        while (getline(archivo, linea)){
+            if (linea[0] == '#') continue;
+            if (linea.empty()) continue;
+            stringstream ss(linea);
+            string clave, valor;
+            getline(ss, clave, '=');
+            getline(ss, valor);
+            if (clave == "liga") {
+                config.name = valor;
+            } else if (clave == "puntos_victoria"){
+                config.wpoints = stoi(valor);
+            } else if (clave == "puntos_derrota"){
+                config.lpoints = stoi(valor);
+            } else if (clave == "puntos_empate"){
+                config.dpoints = stoi(valor);
+            } else if (clave == "equipo"){
+                config.teams.push_back(valor);
+            }
+        }
+        archivo.close();
+        return config;
+    } else {
+        cout << "Error: no se pudo abrir " << ruta << endl;
+        exit(1);
+    }
+}
+
 int main() {
-    cout << "Liga de Futbol - en construccion" << endl;
+    ConfigLiga config = leerConfig("data/config.txt");
+    cout << "Liga: " << config.name << endl;
+    cout << "Equipos: " << config.teams.size() << endl;
     return 0;
 }
